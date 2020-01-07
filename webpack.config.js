51dabe6296c91c26ba879cecf29b7ecdb42fa80f
Daniel Andrade <dcca12@gmail.com>
2020-01-07 10:03:22 -0800

    revert minimize change for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,30 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  optimization: {
+    minimize: false //Update this to true or false
+  }
+};
+
+module.exports = {
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  }
+};
+
+
 switch (process.env.NODE_ENV) {
   case 'prod':
   case 'production':
